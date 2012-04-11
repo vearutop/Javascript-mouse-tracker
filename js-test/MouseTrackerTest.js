ad7389bdab35535783e6e11199ccb41a7fd32592
Viacheslav Poturaev <nanopeni@gmail.com>
2012-04-11 18:00:24 +0800

    ...

--- js-test/MouseTrackerTest.js
+++ js-test/MouseTrackerTest.js
@@ -1,61 +1,45 @@
-//MouseTrackerTest = TestCase("MouseTrackerTest");
+MouseTrackerTest = TestCase("MouseTrackerTest");
 
-MouseTrackerTest = makeClass();
+MouseTrackerTest.prototype.tracker = null;
+MouseTrackerTest.prototype.publisher = null;
 
-$.extend(MouseTrackerTest.prototype, {}, {
-    tracker: null,
+MouseTrackerTest.prototype.setUp = function () {
+    this.tracker = MouseTrackSubscriber({
+        /* enable or disable printing debug info to console.log */
+        debug:true,
+        /* url for saving track, track format is concat[floor(timeSinceStartInMs/timeResolution),x,y;] */
+        saveUrl:'http://vearutop.github.com/Javascript-mouse-tracker/js/Terminator.js?track=',
+        /* server saving time interval (in ms) */
+        saveInterval:100,
+        /* min time interval between track frames */
+        timeResolution:30,
+        /* perform server query with empty track */
+        saveEmpty:false
+    });
 
-    setUp:function () {
-        this.tracker = MouseTrackSubscriber({
-            /* enable or disable printing debug info to console.log */
-            debug:true,
-            /* url for saving track, track format is concat[floor(timeSinceStartInMs/timeResolution),x,y;] */
-            saveUrl:'http://vearutop.github.com/Javascript-mouse-tracker/js/Terminator.js?track=',
-            /* server saving time interval (in ms) */
-            saveInterval:100,
-            /* min time interval between track frames */
-            timeResolution:30,
-            /* perform server query with empty track */
-            saveEmpty:false
-        });
+    /* creating mouse move publisher */
+    this.publisher = MousePublisher({debug:false})
+        /* adding track logger and saving */
+        .addSubscriber(this.tracker);
+};
 
-        /* creating mouse move publisher */
-        this.publisher = MousePublisher({debug:false})
-            /* adding track logger and saving */
-            .addSubscriber(this.tracker);
-    },
+MouseTrackerTest.prototype.tearDown = function () {
+    this.publisher.stopListening();
+};
 
-    tearDown: function(){
-        this.publisher.stopListening();
-    },
+MouseTrackerTest.prototype.testTrack = function () {
+    this.publisher.setEvent({pageX:100, pageY:200});
+    assertEquals('last frame x', this.tracker.lastFrame.x, 100);
+    assertEquals('last frame y', this.tracker.lastFrame.y, 2200);
+    assertEquals('track', this.tracker.trackString, '');
 
-    timer: null,
-
-
-    testTrack: function() {
-        this.publisher.setEvent({pageX: 100, pageY: 200});
-
-        // fill track
-        (function(obj){
-            obj.timer = window.setInterval(function(){obj.saveTrack();}, obj.settings.saveInterval);
-        })(this);
-
-        //emulating mouse movement
-
-
-
-
-        jstestdriver.console.info("last frame is ", this.publisher.lastEvent, " ");
-
-        jstestdriver.console.info("x-holder is ", $('#x-holder').html(), " ");
-    },
-
-    end: null
-
-
-});
-
-
+    // emulating mouse movement
+    /*
+     (function(obj){
+     obj.timer = window.setInterval(function(){obj.saveTrack();}, obj.settings.saveInterval);
+     })(this);
+     */
+};
 
 
 /**
